37ee3fbc481e937c44bbd0d5c63cdfab71cd59ec
DanielBMeeker <54420579+DanielBMeeker@users.noreply.github.com>
2020-06-13 15:20:18 -0500

    Create basic_for_loops.py

---
+++ topic1/basic_for_loops.py
@@ -0,0 +1,10 @@
+"""
+Program: basic_for_loops.py
+Author: Daniel Meeker
+Date: 06/13/2020
+
+This program will demonstrate the use of basic for loops
+"""
+#Declare a list of floating point numbers
+#Write a for loop to print each
+#Write a second for loop to print the odd number descending from 99 to 33 (including 99 and 33)
